64b405bf99c92ce0cf7a05db47154d286e5adaff
Setu Shah <setu4993@yahoo.co.in>
2021-01-10 13:32:45 -0800

    ✅Update tokenizer, model tests

--- tests/test_convert_labse_tf_hub_to_pytorch.py
+++ tests/test_convert_labse_tf_hub_to_pytorch.py
@@ -1,18 +1,55 @@
+from numpy import allclose
 from pytest import fixture
 
-import tensorflow_hub as hub
 from bert.tokenization.bert_tokenization import FullTokenizer
+from transformers import BertTokenizerFast
+
+from convert_labse_tf_pt import (
+    convert_tf2_hub_model_to_pytorch,
+    labse_tokenizer,
+    tf_model,
+)
+
+SIMILAR_SENTENCES = ["Hi, how are you?", "Hello, how are you doing?"]
+TOKENIZER_ATTRIBUTES = {"padding": "max_length", "max_length": 64}
+TOLERANCE = 0.1
 
 
-@fixture
-def tf_model():
-    return hub.load("https://tfhub.dev/google/LaBSE/1")
+def test_convert_tokenizer():
+    hub_model = tf_model()
+
+    tf_tokenizer = FullTokenizer(
+        hub_model.vocab_file.asset_path.numpy(), hub_model.do_lower_case.numpy()
+    )
+    tf_tokenized = tf_tokenizer.convert_tokens_to_ids(
+        tf_tokenizer.tokenize(SIMILAR_SENTENCES[0])
+    )
+
+    hf_tokenizer = labse_tokenizer(hub_model)
+    hf_tokenized = hf_tokenizer(SIMILAR_SENTENCES[0], add_special_tokens=False)
+    assert tf_tokenized == hf_tokenized.input_ids
 
 
-def test_convert_model(tf_model):
-    pass
+def test_convert_model(tf_model, labse_tokenizer):
+    pt_tokenized = labse_tokenizer(
+        SIMILAR_SENTENCES, return_tensors="pt", **TOKENIZER_ATTRIBUTES
+    )
+    model = convert_tf2_hub_model_to_pytorch()
 
+    pt_labse_output = model(**pt_tokenized)
+    pt_output = pt_labse_output.pooler_output
 
-def test_convert_tokenizer(tf_model):
-    # tf_tokenizer = FullTokenizer()
-    pass
+    tf_tokenized = labse_tokenizer(
+        SIMILAR_SENTENCES, return_tensors="tf", **TOKENIZER_ATTRIBUTES
+    )
+    tf_labse_output = tf_model(
+        [
+            tf_tokenized.input_ids,
+            tf_tokenized.attention_mask,
+            tf_tokenized.token_type_ids,
+        ]
+    )
+    tf_output = tf_labse_output[0]
+    assert allclose(
+        pt_output.detach().numpy(), tf_output.numpy(), rtol=TOLERANCE, atol=TOLERANCE
+    )
