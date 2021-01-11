cfce310a392232fcd36a2d6ede1d420f6c88866a
Setu Shah <setu4993@yahoo.co.in>
2021-01-10 19:42:57 -0800

    🔥Delete version, test

--- src/convert_labse_tf_pt/__init__.py
+++ src/convert_labse_tf_pt/__init__.py
@@ -1,5 +1,3 @@
-__version__ = "0.1.0"
-
 from .convert import (
     convert_tf2_hub_model_to_pytorch,
     get_labse_model,
--- tests/test_convert_labse_tf_pt.py
+++
@@ -1,5 +0,0 @@
-from convert_labse_tf_pt import __version__
-
-
-def test_version():
-    assert __version__ == "0.1.0"
