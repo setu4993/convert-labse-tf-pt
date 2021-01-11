279800e207126386b848bece20a25a6ef3d83996
Setu Shah <setu4993@yahoo.co.in>
2021-01-10 17:24:22 -0800

    📝Simplify file docstring

--- src/convert_labse_tf_pt/convert_labse_tf_hub_to_pytorch.py
+++ src/convert_labse_tf_pt/convert_labse_tf_hub_to_pytorch.py
@@ -1,32 +1,14 @@
-# Copyright 2020 The HuggingFace Team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """
-This script can be used to convert a head-less TF2.x Bert model to PyTorch, as published
-on the official GitHub:
+This script can be used to convert a head-less TF2.x LaBSE model to PyTorch, as
+published on TensorFlow Hub: https://tfhub.dev/google/LaBSE/1.
 
-https://github.com/tensorflow/models/tree/master/official/nlp/bert
-TF2.x uses different variable names from the original BERT (TF 1.4) implementation. The script re-maps the TF2.x Bert
-weight names to the original names, so the model can be imported with Huggingface/transformer.
-You may adapt this script to include classification/MLM/NSP/etc. heads.
+The script re-maps the TF2.x Bert weight names to the original names, so the model can
+be imported with Huggingface/transformer.
 """
 import argparse
-import os
 import re
 from logging import INFO, getLogger
 
-import tensorflow as tf
 import torch
 from tensorflow_hub import load
 from transformers import BertConfig, BertModel, BertTokenizerFast
