5535cad5b057e6eb4a4e37665c00b44d524171cd
Setu Shah <setu4993@yahoo.co.in>
2021-01-15 20:42:04 -0800

    ✅Move repeated structures to fixtures

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,7 +1,11 @@
+from typing import Tuple
+
 from numpy import allclose
+from pytest import fixture
 
 from bert.tokenization.bert_tokenization import FullTokenizer
 from torch import no_grad
+from transformers import BertModel, BertTokenizerFast
 
 from convert_labse_tf_pt import (
     convert_tf2_hub_model_to_pytorch,
@@ -14,17 +18,38 @@ TOKENIZER_ATTRIBUTES = {"padding": "max_length", "max_length": 64}
 TOLERANCE = 0.1
 
 
-def test_convert_tokenizer():
-    hub_model = load_tf_model()
+@fixture(scope="session")
+def hub_model():
+    return load_tf_model()
 
-    tf_tokenizer = FullTokenizer(
+
+@fixture(scope="session")
+def bert_tokenizer(hub_model) -> FullTokenizer:
+    return FullTokenizer(
         hub_model.vocab_file.asset_path.numpy(), hub_model.do_lower_case.numpy()
     )
-    tf_tokenized = tf_tokenizer.convert_tokens_to_ids(
-        tf_tokenizer.tokenize(SIMILAR_SENTENCES[0])
+
+
+@fixture(scope="session")
+def hf_tokenizer(hub_model) -> BertTokenizerFast:
+    return get_labse_tokenizer(hub_model)
+
+
+@fixture(scope="session")
+def model_tokenizer() -> Tuple[BertModel, BertTokenizerFast]:
+    return convert_tf2_hub_model_to_pytorch()
+
+
+def test_convert_tokenizer(hub_model):
+    tokenizer = get_labse_tokenizer(hub_model)
+    assert isinstance(tokenizer, BertTokenizerFast)
+
+
+def test_tokenized(bert_tokenizer, hf_tokenizer):
+    tf_tokenized = bert_tokenizer.convert_tokens_to_ids(
+        bert_tokenizer.tokenize(SIMILAR_SENTENCES[0])
     )
 
-    hf_tokenizer = get_labse_tokenizer(hub_model)
     hf_tokenized = hf_tokenizer(SIMILAR_SENTENCES[0], add_special_tokens=False)
     assert tf_tokenized == hf_tokenized.input_ids
 
