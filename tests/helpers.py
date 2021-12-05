46b25bb8621b4083e0dfa152a8ba50ac792f7833
Setu Shah <setu4993@yahoo.co.in>
2021-12-04 21:48:09 -0800

    ✅Add tests for converting smaller LaBSE model

--- tests/helpers.py
+++ tests/helpers.py
@@ -16,13 +16,14 @@ Sentences = SimpleNamespace(
 )
 
 
-def tf_model_output(sentences, hub_model, hf_tokenizer):
+def tf_model_output(sentences, hub_model, hf_tokenizer, smaller: bool = False):
     sentences = [sentences] if isinstance(sentences, str) else sentences
     tf_tokenized = hf_tokenizer(sentences, return_tensors="tf", padding="max_length")
+    key = "default" if not smaller else "pooled_output"
     return hub_model(
         {
             "input_type_ids": tf_tokenized.token_type_ids,
             "input_mask": tf_tokenized.attention_mask,
             "input_word_ids": tf_tokenized.input_ids,
         }
-    )["default"]
+    )[key]
---
+++ tests/test_conversion_smaller.py
@@ -0,0 +1,116 @@
+from numpy import allclose
+from pytest import fixture, mark
+
+from torch import from_numpy
+from transformers import FlaxBertModel, TFBertModel
+
+from convert_labse_tf_pt import (
+    MODEL_TOKENIZER,
+    convert_tf2_hub_model_to_pytorch,
+    get_embedding,
+    load_tf_model,
+    save_labse_models,
+    similarity,
+)
+
+from .helpers import Sentences, tf_model_output
+
+TOLERANCE = 0.01
+
+TF_SMALLER_SAVED_MODEL = None
+
+
+@fixture(scope="session")
+def smaller_hub_model():
+    return load_tf_model(tf_saved_model=TF_SMALLER_SAVED_MODEL, smaller=True)
+
+
+@fixture(scope="session")
+def smaller_model_tokenizer() -> MODEL_TOKENIZER:
+    return convert_tf2_hub_model_to_pytorch(
+        tf_saved_model=TF_SMALLER_SAVED_MODEL, smaller=True
+    )
+
+
+def test_embeddings_converted_model(
+    sentences,
+    smaller_hub_model,
+    smaller_model_tokenizer: MODEL_TOKENIZER,
+):
+    (model, hf_tokenizer) = smaller_model_tokenizer
+    pt_output = get_embedding(sentences, model, hf_tokenizer).pooler_output
+    tf_output = tf_model_output(
+        sentences, smaller_hub_model, hf_tokenizer, smaller=True
+    )
+
+    assert allclose(pt_output.detach().numpy(), tf_output.numpy(), atol=TOLERANCE)
+
+
+def test_embeddings_all_converted_models(
+    sentences,
+    smaller_hub_model,
+    smaller_model_tokenizer,
+    tmp_path,
+):
+    # Create models.
+    (pt_model, hf_tokenizer) = smaller_model_tokenizer
+    save_labse_models(
+        pt_model, hf_tokenizer, output_path=tmp_path, huggingface_path=True
+    )
+
+    # TF Hub output.
+    hub_output = tf_model_output(
+        sentences, smaller_hub_model, hf_tokenizer, smaller=True
+    )
+
+    pt_output = get_embedding(sentences, pt_model, hf_tokenizer).pooler_output
+
+    tf_output = TFBertModel.from_pretrained(tmp_path)(
+        hf_tokenizer(sentences, return_tensors="tf", padding="max_length")
+    ).pooler_output
+
+    flax_output = FlaxBertModel.from_pretrained(tmp_path)(
+        **hf_tokenizer(sentences, return_tensors="jax", padding="max_length")
+    ).pooler_output
+
+    # Verify all combinations produce equivalent output embeddings.
+    numpy_arrays = [
+        hub_output.numpy(),
+        pt_output.detach().numpy(),
+        tf_output.numpy(),
+        flax_output,
+    ]
+    for array1 in numpy_arrays:
+        for array2 in numpy_arrays:
+            assert allclose(array1, array2, atol=TOLERANCE)
+
+
+@mark.parametrize(
+    "sentences1,sentences2",
+    [
+        (Sentences.english, Sentences.italian),
+        (Sentences.english, Sentences.japanese),
+        (Sentences.italian, Sentences.japanese),
+    ],
+)
+def test_similarity_converted_model(
+    sentences1,
+    sentences2,
+    smaller_hub_model,
+    smaller_model_tokenizer: MODEL_TOKENIZER,
+):
+    (model, hf_tokenizer) = smaller_model_tokenizer
+    pt_output1 = get_embedding(sentences1, model, hf_tokenizer).pooler_output
+    pt_output2 = get_embedding(sentences2, model, hf_tokenizer).pooler_output
+    tf_output1 = tf_model_output(
+        sentences1, smaller_hub_model, hf_tokenizer, smaller=True
+    )
+    tf_output2 = tf_model_output(
+        sentences2, smaller_hub_model, hf_tokenizer, smaller=True
+    )
+
+    pt_similarity = similarity(pt_output1, pt_output2)
+    tf_similarity = similarity(
+        from_numpy(tf_output1.numpy()), from_numpy(tf_output2.numpy())
+    )
+    assert allclose(pt_similarity, tf_similarity, atol=TOLERANCE)
